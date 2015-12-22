f7c232a4f085a85d3937ca35619c5c54289efd4c
gTopa <gtopa@stuy.edu>
2015-12-22 13:09:25 -0500

    tested, have problems

--- Barcode/BarCode.java
+++ Barcode/BarCode.java
@@ -2,7 +2,7 @@ public class BarCode implements Comparable{
     // instance variables
     private String _zip;
     private int _checkDigit;
-    private static final String[] key=["||:::"," :::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:","|:|::"];
+    private static final String[] key={"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:","|:|::"};
 
     // constructors
     //precondtion: zip.length() = 5 and zip contains only digits.
@@ -15,7 +15,10 @@ public class BarCode implements Comparable{
     }
 
     // postcondition: Creates a copy of a bar code.
-    public BarCode(BarCode x){}
+    public BarCode(BarCode x){
+	_zip=x.getZip();
+	_checkDigit=checkSum();
+    }
 
 
     //post: computes and returns the check sum for _zip
@@ -31,20 +34,26 @@ public class BarCode implements Comparable{
     //ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
     public String toString(){
 	String out=_zip+_checkDigit+"   ";
-	for (int i=0;i<zip.length();i++){
+	for (int i=0;i<_zip.length();i++){
 	    out+=key[_zip.charAt(i)];
 	}
 	return out;
     }
 
 
-    public boolean equals(Object other){}
+    public boolean equals(Object other){
+	return this==other||(other instanceof BarCode && (_zip.equals((BarCode)(other).getZip()))); 
+    }
     // postcondition: false if the object is not a BarCode, 
     // false if it is a non-matching barcode
     // true when they match.
 
 
-    public int compareTo(Comparable other){}
+    public int compareTo(Comparable other){
+	return this.toString().compareTo(other.toString());
+    }
     // postcondition: compares the zip + checkdigit 
-
+    public String getZip(){
+	return _zip;
+    }
 }
---
+++ Barcode/Driver.java
@@ -0,0 +1,6 @@
+public class Driver{
+    public static void main(String[]args){
+	BarCode b1=new BarCode("11111");
+	System.out.println(b1);
+    }
+}
