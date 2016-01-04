f053c5232788e5a0357836ff0724352c0d124193
gTopa <gtopa@stuy.edu>
2016-01-04 13:09:34 -0500

    Created Sorts and started selection

---
+++ hwSorts/Sorts.java
@@ -0,0 +1,38 @@
+public class Sorts{
+    public static void printArray(int[] data){
+	if (data.length==0){
+	    System.out.println("[]");
+	}else{
+	    String out="[";
+	    for (int i=0;i<data.length;i++){
+		out+=data[i]+", ";
+	    }
+	    System.out.println(out.substring(0,out.length()-2)+"]");
+	}
+    }
+    public static void insertion(int[] data){
+	int temp=0;
+	for(int i=1;i<data.length;i++){
+	    temp=data[i];
+	    data[shift(i,data)]=temp;
+	}
+    }
+    public static int shift(int pos, int[] data){
+	int temp=data[pos];
+	for (int i=pos-1;i>=0;i--){
+	    if(data[i]>temp){
+		data[i+1]=data[i];
+	    }else{
+		return i+1;
+	    }
+	}
+	return 0;
+    }
+    public static void selection(int[] data){
+	int min=Integer.maxInt();
+	for (int start=0;start<data.length();start++){
+	    for (int i=0;i<data.length();i++){
+	    }
+	}
+    }
+}
